20b62f7469e377afc1965d213f871ffd58f84f1f
Mathew Desjardins <desjardinsm@etsu.edu>
2026-09-24 18:13:02 -0400

    Start of my program!

---
+++ TripCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿//Part 1: Road Trip
+
+//Questions and Answer
+Console.Write("How many miles for your trip? ");
+double tripMiles = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("How many miles per gallon can you car go? ");
+double milePerGallon = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("How much did you pay for gas? ");
+double pricePerGallon = Convert.ToDouble(Console.ReadLine());
+
+//Calculations for Part 1
+double gallonsNeeded = tripMiles / milePerGallon;
+double fuelCost = gallonsNeeded * pricePerGallon;
+  
+//Print the calculations
+Console.WriteLine("Gallons needed: " + gallonsNeeded.ToString("F2"));
+Console.WriteLine("Fuel cost: " + fuelCost.ToString("C"));
